7ee936f7d15128513f8e42faa80f4eb7f4033f30
Avi Mosseri <evan.mosseri@gmail.com>
2021-06-18 00:33:33 -0400

    refactoring

--- bashin.sh
+++ bashin.sh
@@ -1,6 +1,15 @@
 POD_NAME="node-exec-$RANDOM"
 NODE_NAME=$1
 
+
+function cleanup {
+    kubectl -n kube-system delete pod $POD_NAME
+}
+
+trap cleanup EXIT
+
+
+
 NODE_LOOKUP=$(kubectl get node $1 -o json 2>&1)
 if [[ $NODE_LOOKUP == *"not found"* ]]; then
     printf "Node '%s' Not Found\n" $NODE_NAME
@@ -46,11 +55,5 @@ spec:
 EOF
 
 kubectl -n kube-system wait -l pod-id=$POD_NAME --for=condition=ready pod --timeout=600s
-
-function cleanup {
-    kubectl -n kube-system delete pod $POD_NAME
-}
-
-trap cleanup EXIT
 
 kubectl -n kube-system exec -it $POD_NAME -- /bin/bash
